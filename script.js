8fbb0a16434a991e67b7036cdceda34ba1c491e7
mohanedSalaheldin <mohnsd1162002@gmail.com>
2025-04-25 05:53:54 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function calctemp() {
+    var c = document.getElementById("celsius").value;
+    var f = (c * 9/5) + 32;
+    document.getElementById("fahrenheit").value= f;
+    
+}
+function calctemp() {
+    var c = document.getElementById("celsius").value;
+    var f = (c * 9/5) + 32;
+    document.getElementById("fahrenheit").value= f;
+    
+}
+function calctemp() {
+    var c = document.getElementById("celsius").value;
+    var f = (c * 9/5) + 32;
+    document.getElementById("fahrenheit").value= f;
+    
+}
+
